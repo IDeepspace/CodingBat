b13fa714380b42f322ade45c4289b8c6f64f8f24
cxin <cxin@Mac.local>
2018-05-25 21:55:41 +0800

    add countHi

---
+++ src/String_2/CountHi.java
@@ -0,0 +1,26 @@
+/*
+Return the number of times that the string "hi" appears anywhere in the given string.
+
+
+countHi("abc hi ho") → 1
+countHi("ABChi hi") → 2
+countHi("hihi") → 2*/
+
+package String_2;
+
+public class CountHi {
+
+    public static void main(String[] args) {
+        CountHi countHi = new CountHi();
+        int answer = countHi.countHi("abc hi ho");
+        System.out.println(answer);
+    }
+
+    public int countHi(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length()-1; i++)
+            if (str.substring(i, i+2).equals("hi"))
+                count += 1;
+        return count;
+    }
+}
