8356eacb96a3d9f49e516b9fded7f2eaa2ddfe90
cxin <cxin@Mac.local>
2018-05-26 14:15:56 +0800

    countHi

--- src/String_2/CountHi.java
+++ src/String_2/CountHi.java
@@ -19,8 +19,9 @@ public class CountHi {
     public int countHi(String str) {
         int count = 0;
         for (int i = 0; i < str.length()-1; i++)
-            if (str.substring(i, i+2).equals("hi"))
+            if (str.substring(i, i+2).equals("hi")){
                 count += 1;
+            }
         return count;
     }
 }
